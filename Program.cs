86af3151c712fb02f25fcefc6a807805a6377acf
jxy35010 <54958588+jxy35010@users.noreply.github.com>
2019-09-10 21:01:41 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _111
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("how many years experience do you have in professional programming?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+                Console.WriteLine("you have "+i+" years of experience");
+
+        }
+    }
+}
---
+++ if statement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experience do you have in professional programming?");
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input == 0) 
+            {
+                Console.WriteLine("you need more practice");
+            }
+
+            if (input == 1)
+            {
+                Console.WriteLine("looks like you have a little experience");
+            }
+            if (input == 2) {
+                Console.WriteLine("wow! you've been doing this for a little while");
+            }
+            else {
+                Console.WriteLine("You are an expert");
+            }
+
+        }
+    }
+}
